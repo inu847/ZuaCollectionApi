f6a7267d0070bee5aef2ed2b4c3b09d19c4e0605
Deviska Nugroho <deviskanugroho1@gmail.com>
2020-11-14 13:44:59 +0700

    test scraping

--- database/seeders/Baju2TableSeeder.php
+++ database/seeders/Baju2TableSeeder.php
@@ -18,7 +18,7 @@ class Baju2TableSeeder extends Seeder
         $baju = [];
         $faker = Faker::create();
         for($i=0;$i<15;$i++){
-            $avatar_path = 'public/storage/avatars';
+            $avatar_path = 'public/storages/avatars';
             $avatar_fullpath = $faker->image( $avatar_path, 200, 250, 'people', true, true, 'people');
             $avatar = str_replace($avatar_path . '/' , '', $avatar_fullpath);
             $baju[$i] = [
